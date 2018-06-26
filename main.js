da0f041c56727262695e17c1c3cde2b47cf5237b
CristianHolguin14 <39769914+CristianHolguin14@users.noreply.github.com>
2018-06-25 20:06:12 -0500

    Css_Josue

---
+++ main.js
@@ -0,0 +1,36 @@
+var fecha_solicitud="25-06-2018";
+
+var nombre="Pepito Perez";
+
+var fecha_nacimiento="14-01-2004";
+
+var correo="alguien@gmail.com";
+
+var telefono="5557776";
+
+var genero="Masculino";
+
+var departamento="antioquia";
+
+var Ciudad="medellin";
+
+var direccion="Cra 22 #22c -22";
+
+var tipo_de_domicilio="casa";
+
+var integrantes_del_domicilio="4";
+
+var porque_quieres_adoptar_una_mascota="porque quiero compania";
+
+var Tienes_experiencia_con_los_animales=true;
+
+var tienes_otras_mascotas=true;
+
+var cuales_son_tus_ingresos_mensuales= 500000;
+
+var Cuantos_ingresos_dispones_para_tu_mascota=30000;
+
+var Cuanto_tiempo_piensas_dedicarle_a_tu_mascota="10 hora al mes";
+
+var quien_cuidara_tu_mascota="mi abuelo";
+
